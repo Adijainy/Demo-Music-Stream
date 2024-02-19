f5a9bd40cc4280b0d0ea7a3ba83c5d6c968fda2f
AdiJainy <adij1222@gmail.com>
2024-02-19 18:07:18 +0530

    added audio tag to play the recieved song. also sent the song to other clients

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,50 @@
 import "./App.css";
 import { io } from "socket.io-client";
-import { useMemo, useState, useEffect } from "react";
+import { useMemo, useState, useEffect, useRef } from "react";
 import { apiConnector } from "./services/apiconnector";
-import {useAudioPlayer} from 'react-use-audio-player'
+
 function App() {
   const socket = useMemo(() => io("http://localhost:3000"), []);
-  const audioPlayer = useAudioPlayer();
-  const [chunks, setChunks] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [trackList, setTrackList] = useState([]);
+  const [songUrl, setSongUrl] = useState("");
+  const audio = useRef();
+  // const audioPlayer = useAudioPlayer();
+  // const [chunks, setChunks] = useState([]);
   useEffect(() => {
     socket.on("connect", () => {
       console.log(socket.id);
     });
 
-    socket.once("songChunk", (chunk) => {
-      setChunks([...chunks, chunk]);
-      console.log("Inside songChunk")
-      console.log("chunk", chunk)
-      
-      console.log("chunks", chunks)
-    })
+    // socket.once("songChunk", (chunk) => {
+    //   setChunks([...chunks, chunk]);
+    //   console.log("Inside songChunk")
+    //   console.log("chunk", chunk)
 
-    
+    //   console.log("chunks", chunks)
+    // })
+
+    socket.on(
+      "sendSong",
+      (songurl) => {
+        console.log("song recieved" + songurl);
+        setSongUrl(songurl);
+      },
+      async () => {
+        console.log("in ");
+        await audio.current.play();
+      }
+    );
 
     return () => {
       socket.disconnect();
     };
   });
 
-  function handlePlay (){
-    const blob = new Blob(chunks, {type: 'audio/mp3'});
-    audioPlayer.play(blob);
-  }
+  // function handlePlay (){
+  //   const blob = new Blob(chunks, {type: 'audio/mp3'});
+  //   audioPlayer.play(blob);
+  // }
 
   //socket events
   // const playSong = (songUrl) => {
@@ -39,8 +53,6 @@ function App() {
   // }
 
   //state variables
-  const [searchQuery, setSearchQuery] = useState("");
-  const [trackList, setTrackList] = useState([]);
 
   const searchHandler = async (search) => {
     const res = await apiConnector(
@@ -53,24 +65,27 @@ function App() {
       },
       null
     );
-    
+
     setTrackList(res.data.tracks);
     return res;
   };
 
-
-
-  const getSong = async(id) => {
-    const res = await apiConnector('GET', `https://spotify81.p.rapidapi.com/download_track?q=${id}&onlyLinks=1`, null, {
-      'X-RapidAPI-Key': 'd56b1f2effmsh5f29610e2682c33p126cd8jsn6d4e17a18aa7',
-      'X-RapidAPI-Host': 'spotify81.p.rapidapi.com'
-    });
-    console.log("Before event emit")
-    // playSong(res.data[0].url);
-    socket.emit("playSong", res.data[0].url);
-    console.log("After event emit")
-  }
-  
+  const getSong = async (id) => {
+    const res = await apiConnector(
+      "GET",
+      `https://spotify81.p.rapidapi.com/download_track?q=${id}&onlyLinks=1`,
+      null,
+      {
+        "X-RapidAPI-Key": "d56b1f2effmsh5f29610e2682c33p126cd8jsn6d4e17a18aa7",
+        "X-RapidAPI-Host": "spotify81.p.rapidapi.com",
+      }
+    );
+    console.log("Before event emit");
+    await setSongUrl(res.data[0].url);
+    socket.emit("playSong", songUrl);
+    console.log("After event emit and before audio play");
+    audio.current.play();
+  };
 
   return (
     <>
@@ -87,19 +102,15 @@ function App() {
           <h6>Search results</h6>
           <ul>
             {trackList.map((item, ind) => (
-              
-                <li key={ind} onClick={() => getSong(item.data.id)}>
-                  <img src={item.data.albumOfTrack.coverArt.sources[0].url} />
-                  {item.data.albumOfTrack.name}{" "}
-                </li>
-              
-              
+              <li key={ind} onClick={() => getSong(item.data.id)}>
+                <img src={item.data.albumOfTrack.coverArt.sources[0].url} />
+                {item.data.albumOfTrack.name}{" "}
+              </li>
             ))}
           </ul>
         </div>
 
-
-        <button onClick={() => handlePlay()}>Play song</button>
+        <audio ref={audio} src={songUrl} controls />
       </div>
     </>
   );
